54b0cb85d87df8460f9385162ea979c46efcb7b7
OkoroKelvin <okorokelvinemoakpooke@yahoo.com>
2022-03-08 11:38:05 +0100

    Bike project setup

---
+++ src/main/java/com/example/globomatics/GlobomaticsBikeApplication.java
@@ -0,0 +1,12 @@
+package com.example.globomatics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobomaticsBikeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GlobomaticsBikeApplication.class, args);
+    }
+
+}
